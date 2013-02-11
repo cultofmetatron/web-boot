7cd47cf98c3d0949ddbc6c4b08c7987d7b7488ac
cultofmetatron <cultofmetatron@aumlogic.com>
2013-02-11 18:36:21 -0500

    added in stick required js files

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ web-boot
 ========
 
 project skeleton to build web design frontends
+
+this repo is just a quick checklist of stuff to
+throw into a rich web application.
+
+TODOS
+*javascript*
+    1.jQuery
+    2.underscore
+    3.backbone.js
+    4.modernizr
+    4.require.js
+    5.main.js //boot file
+
+*stylesheets powered by skeleton*
+
+
---
+++ js/main.js
@@ -0,0 +1,21 @@
+requirejs.config({
+    paths: {
+               jQuery:'/js/libs/jquery-1.9.1',
+               Underscore:'/js/libs/underscore',
+               Modernizr: '/js/libs/modernizr',
+               Backbone: '/js/libs/backbone',
+               text: '/js/libs/text',
+               cs: '/js/libs/cs',
+               templates:'/templates'
+           },
+    shim: {
+                Modernizr:['jQuery'],
+                Backbone: ['jQuery', 'Underscore']
+                Application: ['Backbone']
+
+           }
+})
+
+require(['Application'], function(app) {
+    app.init();
+})
