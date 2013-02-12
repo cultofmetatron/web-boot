6e06b8a11f990f0fd3c905681ca815bacab7a456
cultofmetatron <cultofmetatron@aumlogic.com>
2013-02-11 20:25:34 -0500

    redirection goes through

--- README.md
+++ README.md
@@ -7,13 +7,9 @@ this repo is just a quick checklist of stuff to
 throw into a rich web application.
 
 TODOS
-*javascript*
-    1.jQuery
-    2.underscore
-    3.backbone.js
-    4.modernizr
-    4.require.js
-    5.main.js //boot file
+
+    redirection to index template works now!!
+
 
 *stylesheets powered by skeleton*
 
--- js/router.js
+++ js/router.js
@@ -2,12 +2,20 @@ define(['views/index'], function(IndexView) {
 
     //declare the router
     var router = Backbone.Router.extend({
+        currentView: null,
         routes: {
                     'index':'index'
 
                 },
+        changeView: function(newView) {
+            if(this.currentView != null) {
+                this.currentView.undelegateEvents();
+            }
+            this.currentView = newView;
+            this.currentView.render();
+        },
         index: function() {
-
+            this.changeView(new IndexView())
             console.log('index called');
         }
 
